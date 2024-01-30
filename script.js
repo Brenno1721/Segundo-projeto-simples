23bdbf94fc8037f2807467d1737a97134be1a85b
Brenno Alencar <142350225+Brenno1721@users.noreply.github.com>
2024-01-30 20:32:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const hoursElement = document.getElementById('hours')
+const minutesElement = document.getElementById('minutes')
+const secondsElement = document.getElementById('seconds')
+
+const date = new Date()
+
+const hours = date.getHours
+const minutes = date.getMinutes
+const seconds = date.getSeconds
+
+hoursElement.textContent = hours
+
+console,console.log(hours, minutes, seconds);
